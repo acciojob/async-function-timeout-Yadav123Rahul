251fa4c1f3ee2587c9ff536eafb6752855c157ee
Rahul Yadav <119389899+Yadav123Rahul@users.noreply.github.com>
2023-08-29 12:16:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+ async function showMessageWithDelay() {
+            const textInput = document.getElementById('text');
+            const delayInput = document.getElementById('delay');
+            const outputDiv = document.getElementById('output');
+            
+            const text = textInput.value;
+            const delay = parseInt(delayInput.value);
+
+            if (!text || isNaN(delay)) {
+                outputDiv.textContent = "Please provide valid input.";
+                return;
+            }
+
+            await new Promise(resolve => setTimeout(resolve, delay));
+
+            outputDiv.textContent = text;
+        }
+
+        const btn = document.getElementById('btn');
+        btn.addEventListener('click', showMessageWithDelay);
